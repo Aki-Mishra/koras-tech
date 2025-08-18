ca84e333d70c96b73ec5e3640b5cfa5f96fba149
Aki-Mishra <nightmayre420@gmail.com>
2025-08-18 19:40:52 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en" >
       <head>
 
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <title>Koras tech</title>
       </head>
 
